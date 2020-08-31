52b1d7c923411dc13618bf66eb7e9b9d42f78114
Brent Dunklau <brentdunklau@csitx.com>
2020-08-31 15:34:50 -0500

    removed unused function

--- nodejs/index.js
+++ nodejs/index.js
@@ -60,88 +60,6 @@ app.get('/downloadComposition', function(req, res) {
 
 
 
-app.post('/downloadCompositionORIG', function(req, res) {
-	// See twilio-telepatriot.js : twilioCallback() : the "composition-available" block
-	/**
-	 *  this is what we pass to this function from twilio-vodeo.js:twilioCallback()
-	 * 
-        var formData = {
-			RoomSid: roomSid,
-            twilio_account_sid: twilioAccountSid,
-            domain: 'video.twilio.com',
-            MediaUri: req.body.MediaUri,
-            CompositionSid: req.body.CompositionSid,
-            Ttl: 3600,
-            firebase_functions_host: req.query.firebase_functions_host,
-			firebase_function: '/downloadComplete',
-            twilio_auth_token: twilioAuthToken
-         };
-	 */
-
-    var twilioUrl = `https://${req.body.twilio_account_sid}:${req.body.twilio_auth_token}@${req.body.domain}${req.body.MediaUri}?Ttl=${req.body.Ttl}`
-	var compositionFile = `~/videos/${req.body.CompositionSid}.mp4`
-
-
-    // ref:  https://stackoverflow.com/questions/44896984/what-is-way-to-download-big-file-in-nodejs
-    progress(request(twilioUrl, {/* parameters */}))
-    .on('progress', function(state) {
-	    /**
-		 * We COULD call back to a firebase function as the file is downloading but I don't see the value in that
-		 * right now  8/29/20
-		 */
-		/************** 
-	    request.post({
-				url: callbackUrl, // <- /downloadComplete would be the wrong url though
-				formData:JSON.stringify({state: state})
-	        },
-		    function(err, httpResponse, body) {
-		        if(err) {
-			        res.send(JSON.stringify({error: err, when: 'during progress'}))
-		        }
-	        }
-		)
-		****************/
-	})
-	.on('error', function(err) {
-	    if(err) {
-	        res.send(JSON.stringify({error: err, when: 'on error'}))	
-	    }
-	})
-	.on('end', function() {
-		//res.send(JSON.stringify({response: 'download complete'}))
-	})
-	.pipe(
-	    fs.createWriteStream(compositionFile).on('finish', function() {
-			/**********************
-			request.post(
-				{
-					url: `https://${req.body.firebase_functions_host}${req.body.firebase_function}`, 
-					formData:JSON.stringify({compositionFile: compositionFile, 
-											 RoomSid: req.body.RoomSid,
-											 tempEditFolder:  `~/videos/${req.body.CompositionSid}`,
-											 downloadComplete: true})
-				},
-				function(err, httpResponse, body) { 
-					if(err) { 
-						res.send(JSON.stringify({error: err, when: 'on finsish'})) 
-					}	
-				}
-			)
-            ***********************/
-			res.send(JSON.stringify({compositionFile: compositionFile})) // could probably just return {res: 'ok'}
-		})
-	)
-	
-	
-    //res.send(JSON.stringify({response: 'ok'}))
-
-}) // end app.get(/downloadComposition)
-
-
-
-
-
-
 /**
  * Called from twilio-video.js:downloadVideo()
  * See the page "Marking Time"
@@ -235,37 +153,6 @@ app.all('/uploadToFirebaseStorage', async function(req, res) {
 
 	return res.status(200).send(JSON.stringify({"result": `uploaded ${filename}`}))
 
-
-
-
-
-
-    /***********
-	// Enable Storage
-	var gcs = gcloud.storage({
-		projectId: 'yourvotecounts-bd737',
-		keyFilename: '/home/bdunklau/yourvotecounts-bd737-980dde8224a5.json'
-	});
-
-	// Reference an existing bucket.
-    var bucket = gcs.bucket('yourvotecounts-bd737.appspot.com');
-
-    // Upload a local file to a new file to be created in your bucket.
-    bucket.upload('/home/bdunklau/videos/CJb499b3f2ad448d93e01f39cbdcc95219-output.mp4', function(err, file) {
-      if (!err) {
-        // video is now in your bucket.
-        return res.status(200).send(JSON.stringify({"result": "uploaded /home/bdunklau/videos/CJb499b3f2ad448d93e01f39cbdcc95219-output.mp4"}))
-	  }
-	  else res.status(500).send(JSON.stringify({"error": err}))
-	});
-	**************/
-
-	// Download a file from your bucket.
-	/**** 
-    bucket.file('giraffe.jpg').download({
-      destination: '/photos/zoo/giraffe.jpg'
-	}, function(err) {});
-	*****/
 })
 
 
