ab3065db5dcc5beba03ee53c6ded29ea25ae1b4b
Brent Dunklau <brentdunklau@csitx.com>
2020-12-04 02:17:43 -0600

    still testing

--- nodejs/index.js
+++ nodejs/index.js
@@ -480,21 +480,21 @@ app.all('/uploadToFirebaseStorage', async function(req, res) {
 		console.log('inside _.each()')
 		// Uploads a local file to the bucket
 		let folder = req.body.CompositionSid
-		// await storage.bucket(bucketName).upload(file.path, {
-		// 	destination: folder+"/"+file.name,
-		// 	// Support for HTTP requests made with `Accept-Encoding: gzip`
-		// 	gzip: true,
-		// 	// By setting the option `destination`, you can change the name of the
-		// 	// object you are uploading to a bucket.
-		// 	metadata: {
-		// 		// Enable long-lived HTTP caching headers
-		// 		// Use only if the contents of the file will never change
-		// 		// (If the contents will change, use cacheControl: 'no-cache')
-		// 		cacheControl: 'public, max-age=31536000',
-		// 	},
-		// });
+		await storage.bucket(bucketName).upload(file.path, {
+			destination: folder+"/"+file.name,
+			// Support for HTTP requests made with `Accept-Encoding: gzip`
+			gzip: true,
+			// By setting the option `destination`, you can change the name of the
+			// object you are uploading to a bucket.
+			metadata: {
+				// Enable long-lived HTTP caching headers
+				// Use only if the contents of the file will never change
+				// (If the contents will change, use cacheControl: 'no-cache')
+				cacheControl: 'public, max-age=31536000',
+			},
+		});
 
-		// await storage.bucket(bucketName).file(folder+"/"+file.name).makePublic();
+		await storage.bucket(bucketName).file(folder+"/"+file.name).makePublic();
 		storageItems.push({bucketName: bucketName, folder: folder, filename: file.name})
 
 	})
