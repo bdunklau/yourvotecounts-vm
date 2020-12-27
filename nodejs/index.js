b82ab59331b573bca3ecfae476ef899222713bef
Brent Dunklau <brentdunklau@csitx.com>
2020-12-27 15:02:40 -0600

    added cors support

--- nodejs/index.js
+++ nodejs/index.js
@@ -6,6 +6,8 @@ var request = require('request')
 var progress = require('request-progress')
 var fs = require('fs')
 const path = require('path')
+var cors = require('cors');
+
 
 var bodyParser = require('body-parser')
 // Imports the Google Cloud client library
@@ -43,25 +45,8 @@ function getUrl(req) {
 }
 
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+// use it before all route definitions
+app.use(cors({origin: 'http://localhost:4200'}));
 
 
 
