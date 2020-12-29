34da773728e5b01a9b4b4c5b634962ffd859abdd
Brent Dunklau <brentdunklau@csitx.com>
2020-12-28 21:40:25 -0600

    simplify /ping

--- nodejs/index.js
+++ nodejs/index.js
@@ -796,19 +796,7 @@ app.all('/env', async function(req, res) {
  */
 app.all('/ping', function(req, res) {
 
-	setTimeout(async () => {
-		// console.log((new Date())+' /ping called')
-		// // yourvotecounts : admin.js : /pingVm
-		// let callbackUrl = `https://${req.query.firebase_functions_host}/${req.query.function}`
-		// console.log(`${new Date()} callback:  ${callbackUrl}`)
-		// const theResponse = await fetch(callbackUrl);
-		// const html = await theResponse.text();
-		// console.log(`${new Date()} callback:  ${callbackUrl} - RESPONSE: ${html}`)
-
-		return res.status(200).send(JSON.stringify({"ping": "ping back"}))
-
-	}, 10000)
-
+	return res.status(200).send(JSON.stringify({"ping": "ping back"}))
 
 })
 
