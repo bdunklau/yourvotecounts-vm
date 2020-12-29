8dd19ae20982422e1d49b4c8f5074a6a4874d0d8
Brent Dunklau <brentdunklau@csitx.com>
2020-12-28 20:23:13 -0600

    just testing

--- nodejs/index.js
+++ nodejs/index.js
@@ -797,13 +797,13 @@ app.all('/env', async function(req, res) {
 app.all('/ping', function(req, res) {
 
 	setTimeout(async () => {
-		console.log((new Date())+' /ping called')
-		// yourvotecounts : admin.js : /pingVm
-		let callbackUrl = `https://${req.query.firebase_functions_host}/${req.query.function}`
-		console.log(`${new Date()} callback:  ${callbackUrl}`)
-		const theResponse = await fetch(callbackUrl);
-		const html = await theResponse.text();
-		console.log(`${new Date()} callback:  ${callbackUrl} - RESPONSE: ${html}`)
+		// console.log((new Date())+' /ping called')
+		// // yourvotecounts : admin.js : /pingVm
+		// let callbackUrl = `https://${req.query.firebase_functions_host}/${req.query.function}`
+		// console.log(`${new Date()} callback:  ${callbackUrl}`)
+		// const theResponse = await fetch(callbackUrl);
+		// const html = await theResponse.text();
+		// console.log(`${new Date()} callback:  ${callbackUrl} - RESPONSE: ${html}`)
 
 		return res.status(200).send(JSON.stringify({"ping": "ping back"}))
 
